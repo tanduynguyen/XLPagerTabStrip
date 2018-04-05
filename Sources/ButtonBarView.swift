db6e0e8b04d768f0ae286cec550be71c544dc324
Duy Nguyen <tanduy2506@gmail.com>
2018-04-05 16:19:41 +0700

    Change contentOffsetForCell to center

--- Sources/ButtonBarView.swift
+++ Sources/ButtonBarView.swift
@@ -159,9 +159,9 @@ open class ButtonBarView: UICollectionView {
             alignmentOffset = leftAlignmentOffset + (rightAlignmentOffset - leftAlignmentOffset) * CGFloat(progress) - cellHalfWidth
         }
         
-        var contentOffset = cellFrame.origin.x - alignmentOffset
-        contentOffset = max(0, contentOffset)
-        contentOffset = min(contentSize.width - frame.size.width, contentOffset)
+        let contentOffset = cellFrame.origin.x - alignmentOffset
+//        contentOffset = max(0, contentOffset)
+//        contentOffset = min(contentSize.width - frame.size.width, contentOffset)
         return contentOffset
     }
     
